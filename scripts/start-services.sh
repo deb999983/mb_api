4cf81d761518a735071bc221fb5a2cc8b0d3aa88
Debasish Mohapatra <debasishm@datetheramp.com>
2020-02-06 21:54:54 +0530

    wait for mysql should run inside container.

--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -4,7 +4,7 @@ echo "Starting services"
 docker-compose up -d
 
 echo "Waiting for mysql to be ready"
-source wait-for-mysql.sh "database"
+source exec -i -t mb_api_container ./home/app/mb_api/scripts/wait-for-mysql.sh "database"
 
 echo "Collecting static files"
 docker exec -i -t mb_api_container python3 /home/app/mb_api/manage.py collectstatic
